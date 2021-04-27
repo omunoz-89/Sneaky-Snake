59145cb30a0356eae927f4e96b6369dbb364ee2f
omunoz <steven.120189@gmail.com>
2021-04-27 17:01:11 -0400

    Changed snake and pellets to circles. Working on collison detection with pellets.

--- README.md
+++ README.md
@@ -5,17 +5,13 @@ Explanation of the game that was built and the deployed link will go here.
 
 
 # HOW TO PLAY
-- Start off as a one cell snake, sneak around fixed obticles to pick up grow pellets.
+- Start off as a one cell snake, sneak around fixed obtacles to pick up grow pellets.
 - As you pick up grow pellets the snake will grow and another grow pellet will spawn randomly
-- Try and collect as many grow pellets as possible while avoiding obstacle or walls.
+- Try and collect as many grow pellets as possible while avoiding obstacles.
 - If the snake crashes againts walls or obstacles the game is over.
 
 ## Start Up Screen:
 Images of the start up screen go here 
-
-# HOW TO INSTALL
-
-- Work in progress
 
 
 # HOW IT WORKS
@@ -25,6 +21,7 @@ Use W,A,S,D or Up, Down, Left, Right arrow keys.
 # FUTURE CONSIDERATIONS
 - Increase velocity the longer you're alive
 - Increase velocity the longer the snake is
+- Add walls that
 - Levels with random obstacles spawned
 
 
--- Script/app.js
+++ Script/app.js
@@ -2,8 +2,8 @@ const canvas = document.querySelector('#game');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ctx = canvas.getContext('2d');
-let speed = 5;
-const grid = 30;
+let speed = 8;
+const grid = 50;
 const scale = canvas.width / grid;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
@@ -11,18 +11,47 @@ const columns = canvas.width / scale;
 
 // ====================== Snake ======================= //
 function Snake() {
-    this.x = scale * 15;
-    this.y = scale * 15;
+    // this.x = Math.round(scale * 25);
+    // this.y = Math.round(scale * 25);
+    this.x = scale * 25;
+    this.y = scale * 25;
     this.xSpeed = 0;
     this.ySpeed = scale * -1;
     this.alive = true;
+    this.eaten = 0;
+    this.body = 0;
     this.render = function() {
+        // ctx.fillStyle = 'rgb(204, 213, 230)';
+        // ctx.fillRect(this.x, this.y, scale, scale);
+        ctx.beginPath();
+        ctx.arc (this.x, this.y, scale, 0, Math.PI * 2, false);
+        // ctx.strokeStyle = 'rgb(59, 84, 131';
         ctx.fillStyle = 'rgb(204, 213, 230)';
-        ctx.fillRect(this.x, this.y, scale, scale);
+        // ctx.lineWidth = 10;
+        ctx.closePath();
+        // ctx.stroke();
+        ctx.fill();
+        // console.log(this.x, this.y)
     }
-    this.loop = function(){
+    this.loop = function() {
         this.x += this.xSpeed;
         this.y += this.ySpeed
+    }
+    this.border = function() {
+        if (this.x > canvas.width) {
+            this.x = 0;
+        } else if (this.x < 0) {
+            this.x = canvas.width
+        } else if (this.y > canvas.height) {
+            this.y = 0;
+        } else if (this.y < 0) {
+            this.y = canvas.height
+        }
+    }
+    this.grow = function() {
+        if (this.x === pellet.x && this.y === pellet.y) {
+            console.log('Ate a pellet')
+        }
     }
 }
 
@@ -37,15 +66,28 @@ function Pellet() {
     this.x;
     this.y;
     
+    // this.emptyCell = function() {
+    //     this.x = (Math.round((Math.floor(Math.random() * columns - 1) + 1) * scale));
+    //     this.y = (Math.round((Math.floor(Math.random() * rows - 1) + 1) * scale));
+    //     console.log(this.x, this.y);
+    
+    // }
+
     this.emptyCell = function() {
         this.x = (Math.floor(Math.random() * columns - 1) + 1) * scale;
         this.y = (Math.floor(Math.random() * rows - 1) + 1) * scale;
+        console.log(this.x, this.y);
     
     }
     
     this.render = function() {
+        // ctx.fillStyle = 'rgb(250, 244, 158)';
+        // ctx.fillRect(this.x, this.y, scale, scale)
+        ctx.beginPath();
+        ctx.arc (this.x, this.y, scale, 0, Math.PI * 2, false);
         ctx.fillStyle = 'rgb(250, 244, 158)';
-        ctx.fillRect(this.x, this.y, scale, scale)
+        ctx.closePath();
+        ctx.fill();
     }
     }
 
@@ -56,6 +98,31 @@ function Pellet() {
 
 
 // ====================== Movement ======================= //
+
+// function movementHandler(e){
+//     if(e.which == 83 || e.which == 40){
+//         snake.xSpeed = 0;
+//         snake.ySpeed = Math.round(scale * 1);
+//     } else if (e.which == 87 || e.which == 38){
+//         snake.xSpeed = 0;
+//         snake.ySpeed = Math.round(-scale * 1);
+//     }else if (e.which == 65 || e.which == 37){
+//         snake.xSpeed = Math.round(-scale * 1);
+//         snake.ySpeed = 0;
+//     }else if (e.which == 68 || e.which == 39){
+//         snake.xSpeed = Math.round(scale * 1);
+//         snake.ySpeed = 0;
+//     } else if (e.which == 16){
+//         snake.xSpeed = 0;
+//         snake.ySpeed = 0;
+//     } else if (e.which == 27){
+//         snake.x = scale * 15;
+//         snake.y = scale * 15;
+//         pellet.emptyCell();
+//         pellet.render();
+//     } 
+// }
+
 
 function movementHandler(e){
     if(e.which == 83 || e.which == 40){
@@ -78,10 +145,8 @@ function movementHandler(e){
         snake.y = scale * 15;
         pellet.emptyCell();
         pellet.render();
-    }
+    } 
 }
-
-
 
 
 // ====================== Draw Game ======================= //
@@ -91,13 +156,13 @@ function drawGame() {
     snake = new Snake();
     pellet = new Pellet();
     pellet.emptyCell();
-
     window.setInterval(function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         pellet.render();
         snake.loop();
         snake.render();
-
+        snake.border();
+        snake.grow();
     }, 1000 / speed);
 };
 
@@ -132,6 +197,9 @@ startButton.addEventListener('click', function() {
     startScreen.style.zIndex = '0';
     drawGame();
     window.addEventListener('keydown', movementHandler, false);
+    // canvas.style.border = '1px solid rgb(233, 143, 143)';
+    // canvas.width = window.innerWidth - 20;
+    // canvas.height = window.innerHeight - 20;
 
 });
 
