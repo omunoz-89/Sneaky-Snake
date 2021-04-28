b76b007250eb4f22bb66f8b8588272c874db9770
omunoz <steven.120189@gmail.com>
2021-04-28 17:37:56 -0400

    Added snake on snake collision, reset screen. Changed snake back to rect

--- Script/app.js
+++ Script/app.js
@@ -2,11 +2,16 @@ const canvas = document.querySelector('#game');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ctx = canvas.getContext('2d');
-const scale = 20;
+const grid = 40;
+const scale = canvas.width / grid;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 let speed = 15;
-
+let startButton = document.querySelector('#startButton');
+let startScreen = document.querySelector('#startScreen');
+let resetButton = document.querySelector('#resetButton');
+let resetScreen = document.querySelector('#resetScreen');
+let score = document.querySelector('#pelletH2');
 
 // ====================== Snake ======================= //
 function Snake() {
@@ -14,29 +19,19 @@ function Snake() {
     this.y = scale * 25;
     this.xSpeed = 0;
     this.ySpeed = -scale;
+    this.alive = true;
     this.eaten = 1;
     this.body = [];
     this.render = function() {
         for (let i=0; i<this.body.length; i++) {
-            ctx.beginPath();
-             ctx.arc (this.body[i].x, this.body[i].y, scale - 5, 0, Math.PI * 2, false);
+
              ctx.fillStyle = 'rgb(204, 213, 230)';
-             ctx.lineWidth = 10;
-             ctx.closePath();
-             ctx.fill();
+             ctx.fillRect(this.body[i].x, this.body[i].y, scale - 2, scale - 2);
           }
-        ctx.beginPath();
-        ctx.arc (this.x, this.y, scale, Math.PI * 2, false);
-        ctx.strokeStyle = 'rgb(59, 84, 131';
-        ctx.fillStyle = 'rgb(204, 213, 230)';
-        ctx.lineWidth = 10;
-        ctx.closePath();
-        ctx.stroke();
-        ctx.fill();
     }
     this.loop = function() {
         this.x += this.xSpeed;
-        this.y += this.ySpeed
+        this.y += this.ySpeed;
         for (let i=0; i<this.body.length - 1; i++) {
             this.body[i] = this.body[i+1];
           }
@@ -55,13 +50,23 @@ function Snake() {
         }
     }
     this.grow = function() {
-        if (this.x < pellet.x + 25 && this.x + 25 > pellet.x && this.y < pellet.y + 25 && this.y + 25 > pellet.y) {
+        if (this.x < pellet.x + 25&& 
+            this.x + 50 > pellet.x && 
+            this.y < pellet.y + 25&& 
+            this.y + 50> pellet.y) {
             pellet.emptyCell();
             pellet.render();
             this.eaten++;
-            speed += 1;
         }
     }
+    this.dead = function() {
+        for (let i=2; i<this.body.length; i++) {
+        if (this.body[0].x === this.body[i].x &&
+            this.body[0].y === this.body[i].y) {
+            this.alive = false;
+        }
+    }
+}
 
 }
 
@@ -84,7 +89,7 @@ function Pellet() {
     
     this.render = function() {
         ctx.beginPath();
-        ctx.arc (this.x, this.y, scale, 0, Math.PI * 2, false);
+        ctx.arc (this.x, this.y, scale - 12, 0, Math.PI * 2, false);
         ctx.fillStyle = 'rgb(250, 244, 158)';
         ctx.closePath();
         ctx.fill();
@@ -122,16 +127,8 @@ function movementHandler(e){
         return;
         snake.xSpeed = scale;
         snake.ySpeed = 0;
-    } else if (e.which == 16){
-        snake.xSpeed = 0;
-        snake.ySpeed = 0;
     } else if (e.which == 27){
-        snake.x = scale * 25;
-        snake.y = scale * 25;
-        snake.ySpeed = scale * -1;
-        snake.eaten = 0;
-        pellet.emptyCell();
-        pellet.render();
+        snake.alive = false
     } 
 }
 
@@ -143,15 +140,31 @@ function drawGame() {
     snake = new Snake();
     pellet = new Pellet();
     pellet.emptyCell();
-    window.setInterval(function() {
+let interval = setInterval(function(event) {
+    if (snake.alive == false) {
+        clearInterval(interval);
+        deadSnake();
+        return;
+    } else {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         pellet.render();
         snake.loop();
         snake.render();
         snake.border();
         snake.grow();
+        snake.dead();
+    }
     }, 1000 / speed);
 };
+
+
+
+
+
+// ====================== Speed ======================= //
+
+
+
 
 
 
@@ -163,21 +176,19 @@ function drawGame() {
 
 
 
-
 // ====================== Game Over ======================= //
 
-// function deadSnake() {
-//         startScreen.style.zIndex = '3';
+function deadSnake() {
 
-// }
+    reset.style.zIndex = '3';
+}
 
 
 
 // ====================== Start Button ======================= //
 
 
-let startButton = document.querySelector('#startButton')
-let startScreen = document.querySelector('#startScreen')
+
 
 startButton.addEventListener('click', function() {
     startScreen.style.zIndex = '0';
@@ -186,3 +197,13 @@ startButton.addEventListener('click', function() {
 
 });
 
+
+// ====================== Reset Button ======================= //
+
+
+resetButton.addEventListener('click', function() {
+    resetScreen.style.zIndex = '1';
+    drawGame();
+    window.addEventListener('keydown', movementHandler, false);
+
+});
